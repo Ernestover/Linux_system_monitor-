ff037e9dca7bd373d903cfbc5b4e0f257680b907
Ernestover <ernestastover@icloud.com>
2026-04-24 13:35:38 -0500

    setting up outline for program

---
+++ main.c
@@ -0,0 +1,28 @@
+/**
+ * Ernest Stover
+ * @details system monitor with a hardware displays
+ * @date 4/24/26
+ */
+
+ // I2C address ->
+ 
+ // Button pins 
+ #define RAM_CPU_BUTTON_PIN 38
+ #define TEMP_UPTIME_BUTTON_PIN 40
+
+ // I2C port and pins 
+
+
+ // LCD I2C address 
+
+ // Function stubs 
+
+ // Main loop 
+ int main() 
+ {
+    // setup
+
+    while(1) {
+
+    }
+ }
