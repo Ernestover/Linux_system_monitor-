55ab9726afa9b7ecb303ddfdb31dea18ba856998
Ernest <erneststovera@gmail.com>
2026-05-02 18:44:54 -0500

    found the i2c address for the LCD screen, started on the function stubs for the i2c address

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
-/**
+ /**
  * Ernest Stover
  * @details system monitor with a hardware displays
  * @date 4/24/26
  */
-
- // I2C address ->
  
  // Button pins 
- #define RAM_CPU_BUTTON_PIN 38
- #define TEMP_UPTIME_BUTTON_PIN 40
 
- // I2C port and pins 
+ // i2c port and pins
+ #define I2C_PORT i2c1
+ #define I2C_SDA 3
+ #define I2C_SDA 5
 
-
- // LCD I2C address 
+ // LCD i2c address
+ static const uint8_t LCD_ADDR = 0x27;
 
  // Function stubs 
+ void i2c_send_byte(uint8_t data);
+
 
  // Main loop 
  int main() 
